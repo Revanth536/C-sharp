e3e721d7146cf6921c46e19cf34e17248ecbb9bb
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-02-12 10:59:01 +0530

    Prime Program

---
+++ Prime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.basics
+{
+    internal class Prime
+    {
+        public static void Main(string[] args) {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int fc = 0;
+            for (int i = 1; i <= a; i++) {
+                if (a % i == 0)
+                    fc++;
+            }
+            if (fc == 2)
+                Console.Write("Prime");
+            else
+                Console.Write("Not Prime");
+        }
+    }
+}
