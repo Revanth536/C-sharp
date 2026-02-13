a07f1f409c6e6abd12b7ffcf5d840cfe139781f6
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-02-13 19:05:21 +0530

    Update Prime class to check prime numbers in range
    Refactored the Main method to check for prime numbers in a given range instead of a single number.

--- basics/Prime.cs
+++ basics/Prime.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +8,34 @@ namespace Basics.basics
 {
     internal class Prime
     {
-        public static void Main(string[] args) {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int fc = 0;
-            for (int i = 1; i <= a; i++) {
-                if (a % i == 0)
-                    fc++;
+        public static void Main(string[] args)
+        {
+            // To check wheather a number is Prime or not   
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int fc = 0;
+            //for (int i = 1; i <= a; i++) {
+            //    if (a % i == 0)
+            //        fc++;
+            //}
+            //if (fc == 2)
+            //    Console.Write("Prime");
+            //else
+            //    Console.Write("Not Prime");
+
+            // To check prime number in range
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = a; i <= b; i++) { 
+                int fc = 0;
+                for(int j = 1; j <= i; j++ )
+                {
+                    if(i%j==0)
+                        fc++;
+                }
+                if (fc == 2)
+                    Console.Write(i + " ");
             }
-            if (fc == 2)
-                Console.Write("Prime");
-            else
-                Console.Write("Not Prime");
         }
     }
 }
