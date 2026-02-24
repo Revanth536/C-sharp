85b53623485fa313fabafad1a768f6a7de199ae6
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-02-24 12:35:50 +0530

    Add Freq class to read and print strings

---
+++ basics/String1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.basics
+{
+    internal class Freq
+    {
+        public static void Main(String[] args)
+        {
+            String a=Convert.ToString(Console.ReadLine());
+
+            String b = Convert.ToString(Console.ReadLine());
+
+            String c = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+        }
+    }
+}
